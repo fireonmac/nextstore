5ee1825def192eb80af8f0872533e3f380c59e4a
Paul Noh <fireonmac@gmail.com>
2025-02-06 15:05:20 +0900

    feat: add styled product price

--- src/lib/components/feature/product/ProductCard.tsx
+++ src/lib/components/feature/product/ProductCard.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Card, CardContent, CardHeader } from "@/lib/components/ui/card";
+import ProductPrice from "./ProductPrice";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
@@ -26,7 +27,7 @@ const ProductCard = ({ product }: { product: any }) => {
         <div className="flex-between gap-4">
           <p>{product.rating} stars</p>
           {product.stock > 0 ? (
-            <p className="font-bold">${product.price}</p>
+            <ProductPrice value={Number(product.price)} />
           ) : (
             <p className="text-destructive">Out of Stock</p>
           )}
