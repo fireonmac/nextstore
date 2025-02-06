3ed5080149a90d81dddc6375ef4674d1c5071577
Paul Noh <fireonmac@gmail.com>
2025-02-06 13:18:34 +0900

    feat: add product-list with sample products
    - lib/data folder added (codes related to crud operations on db will go here)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,17 @@
+import sampleData from "@/lib/data/sample-data";
+import ProductList from "@/lib/components/feature/product/product-list";
+
 const HomePage = async () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
