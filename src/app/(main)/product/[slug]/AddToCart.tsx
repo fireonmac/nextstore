e1536e92b7e01aa01c07677c9c09af588050d177
Paul Noh <fireonmac@gmail.com>
2025-04-27 14:49:58 +0900

    feat: prevent interaction while updating quantity of item in cart

--- src/app/(main)/product/[slug]/AddToCart.tsx
+++ src/app/(main)/product/[slug]/AddToCart.tsx
@@ -7,54 +7,78 @@ import { useToast } from '@/lib/hooks/use-toast';
 import { CartItem } from '@/lib/types';
 import { Plus, Minus } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { useTransition } from 'react';
 
-const AddToCart = ({ countInCart, item }: { countInCart: number; item: CartItem }) => {
+const AddToCart = ({
+  countInCart,
+  item,
+}: {
+  countInCart: number;
+  item: CartItem;
+}) => {
   const router = useRouter();
   const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
 
   const handleAddToCart = async () => {
-    // Execute the addItemToCart action
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      // Execute the addItemToCart action
+      const res = await addItemToCart(item);
 
-    // Display appropriate toast message based on the result
-    if (!res.success) {
+      // Display appropriate toast message based on the result
+      if (!res.success) {
+        toast({
+          variant: 'destructive',
+          description: res.message,
+        });
+        return;
+      }
+
       toast({
-        variant: 'destructive',
         description: res.message,
+        action: (
+          <ToastAction
+            className="bg-primary text-white hover:bg-gray-800"
+            onClick={() => router.push('/cart')}
+            altText="Go to cart"
+          >
+            Go to cart
+          </ToastAction>
+        ),
       });
-      return;
-    }
-
-    toast({
-      description: res.message,
-      action: (
-        <ToastAction
-          className="bg-primary text-white hover:bg-gray-800"
-          onClick={() => router.push('/cart')}
-          altText="Go to cart"
-        >
-          Go to cart
-        </ToastAction>
-      ),
     });
   };
 
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    toast({
-      description: res.message,
-      variant: res.success ? 'default' : 'destructive',
+      toast({
+        description: res.message,
+        variant: res.success ? 'default' : 'destructive',
+      });
     });
   };
-  
+
   return countInCart ? (
     <div className="flex items-center">
-      <Button type="button" variant="outline" size="sm" onClick={handleRemoveFromCart}>
+      <Button
+        disabled={isPending}
+        type="button"
+        variant="outline"
+        size="sm"
+        onClick={handleRemoveFromCart}
+      >
         <Minus className="w-4 h-4" />
       </Button>
       <span className="px-2">{countInCart}</span>
-      <Button type="button" variant="outline" size="sm" onClick={handleAddToCart}>
+      <Button
+        disabled={isPending}
+        type="button"
+        variant="outline"
+        size="sm"
+        onClick={handleAddToCart}
+      >
         <Plus className="w-4 h-4" />
       </Button>
     </div>
