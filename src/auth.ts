ba7d7efc1732af2e49afb5819132688d89f19fff
Paul Noh <fireonmac@gmail.com>
2025-05-31 08:38:18 +0900

    feat: protect paths

--- src/auth.ts
+++ src/auth.ts
@@ -93,6 +93,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return session;
     },
     async authorized({ request }) {
+      // Array of regex patterns of protected paths
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname from the req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and on a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
       // Check for cart cookie
       if (!request.cookies.get('sessionCartId')) {
         // Generate cart cookie
